f8c510421b6e106040018d18818169a43e1faa1c
SeriousCats <prokshin2@mail.ru>
2025-11-15 21:51:15 +0300

    Update database.py

--- database.py
+++ database.py
@@ -1,24 +1,22 @@
 import sqlite3
 
 def init_db():
-    conn = sqlite3.connect("data.db")
-    cursor = conn.cursor()
-    cursor.execute("""
-        CREATE TABLE IF NOT EXISTS users (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            ip TEXT,
-            time TEXT,
-            full_name TEXT,
-            username TEXT
-        )
-    """)
-    conn.commit()
-    conn.close()
+    with sqlite3.connect("data.db") as conn:
+        cursor = conn.cursor()
+        cursor.execute("""
+            CREATE TABLE IF NOT EXISTS users (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                ip TEXT,
+                time TEXT,
+                full_name TEXT,
+                username TEXT
+            )
+        """)
+        conn.commit()
 
 def insert_user(ip, time, full_name, username):
-    conn = sqlite3.connect("data.db")
-    cursor = conn.cursor()
-    cursor.execute("INSERT INTO users (ip, time, full_name, username) VALUES (?, ?, ?, ?)",
-                   (ip, time, full_name, username))
-    conn.commit()
-    conn.close()
+    with sqlite3.connect("data.db") as conn:
+        cursor = conn.cursor()
+        cursor.execute("INSERT INTO users (ip, time, full_name, username) VALUES (?, ?, ?, ?)",
+                       (ip, time, full_name, username))
+        conn.commit()
